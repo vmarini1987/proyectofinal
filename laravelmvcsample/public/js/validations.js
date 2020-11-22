c1a410f093a4a68726d9eb138b638d163fb9b9bc
curriculum update <69602152+vmarini1987@users.noreply.github.com>
2020-11-22 08:06:19 +0100

    Add files via upload

--- laravelmvcsample/public/js/validations.js
+++ laravelmvcsample/public/js/validations.js
@@ -1,21 +1,29 @@
-var titulo = document.getElementById('titulo');
-var descripcion = document.getElementById('descripcion');
+const descripcion = document.getElementById('descripcion');
+const contador = document.getElementById('contador');
 
-document.agregar_pelicula.addEventListener("submit", validar);
- 
-function validar(evObject) {
-    evObject.preventDefault();
-    var validacionOK = true;
+descripcion.addEventListener('input', function(e) {
+    const target = e.target;
+    const longitudMax = target.getAttribute('maxlength');
+    const longitudAct = target.value.length;
+    contador.innerHTML = `${longitudAct}/${longitudMax}`;
+});
 
-    if (titulo.value.length < 3 || titulo.value.length > 30) {
-        alert('Usted debe ingresar un Titulo que contenga entre 3 a 30 caracteres');
-        validacionOK = false;
+function validar() {
+
+    var titulo = document.getElementById('titulo');
+    var descripcion = document.getElementById('descripcion');
+    var caratula = document.getElementById('caratula');
     
-    } else if (descripcion.value.length < 5 || descripcion.value.length > 200) {
-        alert('Usted debe ingresar una Descripcion que contenga entre 5 a 2000 caracteres');
-        validacionOK = false;
+    if ( titulo.value.length < 3 || titulo.value.length > 30 ) {
+        alert('Usted debe ingresar un Titulo que contenga entre 3 a 30 caracteres');
+        return false;
+    }
+    
+    if ( descripcion.value.length < 5 || descripcion.value.length > 200 ) {
+        alert('Usted debe ingresar una Descripcion que contenga entre 5 a 200 caracteres');
+        return false;
     }
 
-    if ( validacionOK == true ) { formulario.submit(); }
-
+    return true;
 }
+document.agregar_pelicula.addEventListener("submit", validar);
--- laravelmvcsample/resources/views/createpost.blade.php
+++ laravelmvcsample/resources/views/createpost.blade.php
@@ -13,8 +13,8 @@
         </header>
         <nav>
             <ul>
-            <li><a href="/">Home</a></li>
-            <li><a href="newpost">New Movie</a></li>
+                <li><a href="/">Home</a></li>
+                <li><a href="newpost">New Movie</a></li>
             </ul>
         </nav>    
         <section>   
@@ -28,22 +28,25 @@
                 </div>
             @endif
             <div class="formulario">
-                <form id="nueva_pelicula" name="agregar_pelicula" method="post" action="{{URL::to('/')}}/newpost" enctype="multipart/form-data">
+                <form id="nueva_pelicula" name="agregar_pelicula" method="post" action="{{URL::to('/')}}/newpost" enctype="multipart/form-data" onsubmit="return validar()">
                     {{ csrf_field() }}
                     <div id="input_titulo" class="formulario">            
                         <label for="titulo">Titulo:</label><br>
-                        <input type="text" id="titulo" name="title" value="" required><br>
+                        <input type="text" id="titulo" name="title" value="" required maxlength="30"><br>
                     </div>
                     <div id="input_descripcion" class="formulario">            
                         <label for="descripcion">Descripcion:</label><br>
-                        <textarea id="descripcion" name="description" required></textarea> <br>
+                        <textarea id="descripcion" name="description" required rows="3" maxlength="200"></textarea> <br>
+                        <div id="contador">0/100</div>
+                    </div>
+                    <div class="box">
                     </div>
                     <div id="input_archivo" class="formulario">            
                         <label for="caratula">Caratula:</label>
                         <input type="file" id="caratula" name="cover_name" required><br>
                     </div>
                     <div id="submit_agregar" class="formulario">            
-                        <input onclick="validar()" id="enviar" type="submit" value="Submit">
+                        <input type="submit" id="enviar" onclick="validar()" value="Submit">
                     </div>
                 </form>
             </div>
